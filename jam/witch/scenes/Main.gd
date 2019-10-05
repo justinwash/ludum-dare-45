49431b0bf11bbc7878c0b5282a2df5523f90e938
Emilio Navarro <emilio.mst@gmail.com>
2019-10-05 15:18:52 -0500

    randomly places items within an x and y range

---
+++ jam/witch/scenes/Main.gd
@@ -0,0 +1,31 @@
+extends Node2D
+const Leaf = preload("../items/Leaf/Leaf.gd")
+const Mushroom = preload("../items/Mushroom/Mushroom.gd")
+
+func _ready():
+	for i in range(10):
+		var posVector = getRandomVector2(10, 10)
+		print(posVector)
+		var newItem = getRandomItem()
+		(newItem as Node2D).position = posVector
+		addItemToItems(newItem)
+		
+func getRandomVector2(xMax, yMax) -> Vector2:
+	var xPos = randi()%xMax+1
+	var yPos = randi()%yMax+1
+	return Vector2(xPos, yPos)
+
+func getRandomItem():
+	var items = [Leaf, Mushroom]
+	var index = (randi()%items.size()+1)-1
+	var item = items[index]
+	if (index == 0):
+		print("Making a leaf")
+	else:
+		print("Making a shroom")
+	return item.new()
+
+func addItemToItems(item): 
+	var Items = get_node("Items")
+	Items.add_child_below_node(Items, item)
+	print(Items.get_child_count())
