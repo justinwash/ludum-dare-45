c9233019f21fbd1f7e26130d38e6d7ceeb25bf4b
Nathan Wash <nwash57@gmail.com>
2019-10-04 22:54:27 -0500

    Basic New Inventory

--- jam/witch/Inventory.gd
+++ jam/witch/Inventory.gd
@@ -1,35 +1,17 @@
 extends Node
 
-var ItemQuantityPair = preload("res://ItemQuantityPair.gd")
 var Item = preload("res://Item.gd").new()
-var items := []
 
-func add_item(id, quantity):
-    if quantity > 0:
-        var iqp = get_item_quantity_pair(id)
-        if iqp != null:
-            iqp.quantity += quantity
-        else:
-            iqp = ItemQuantityPair.new()
-            iqp.item = Item.get_item_from_id(0)
-            iqp.quantity = quantity
-            items.push_back(iqp)
-    else:
-        print("Cannot add a negative amount of item")
+func _ready():
+	add_child(Item.get_item_from_id(0))
+
+func add_item(id):
+	for item in get_children():
+		if item.id == id:
+			item.quantity += 1
 
 func remove_item(id, quantity):
     if quantity > 0:
-        var iqp = get_item_quantity_pair(id)
-        if iqp != null and iqp.quantity >= quantity:
-            iqp.quantity -= quantity
-        else:
-            print("Not enough of item in inventory to remove " + quantity)
+        pass
     else:
         print("Cannot remove a negative amount of item")
-
-func get_item_quantity_pair(id):
-	for iqp in items:
-		print(iqp.item.id)
-		if (iqp.item.id == id):
-            return iqp
-	return null
--- jam/witch/Item.gd
+++ jam/witch/Item.gd
@@ -1,11 +1,15 @@
 extends Node
-onready var Mushroom = load("res://items/Mushroom.gd")
 
 var id
 var item_name
+var quantity
 var price
 var description
 
+var types = {
+	"Mushroom": load("res://items/Mushroom.gd")
+}
+
 func get_item_from_id(id):
 	if id == 0:
-		return Mushroom
+		return self.types.Mushroom.new()
