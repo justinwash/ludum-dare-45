7957da861a5664b833d6c945f683929c39703269
Nathan Wash <nwash57@gmail.com>
2019-10-06 20:54:32 -0500

    Added feather item and changed Market texture to Petunia

---
+++ jam/witch/items/Feather/Feather.gd
@@ -0,0 +1,11 @@
+extends "res://inventory/Item.gd"
+
+var tex = preload("res://items/Feather/Feather.png")
+
+func _init():
+	id = 4
+	item_name = "Feather"
+	quantity = 0
+	price = 5
+	description = "It's... a bird-leaf."
+	texture = tex
